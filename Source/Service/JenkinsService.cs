647a61da13463381f2b6241fd7fae1843d87a576
ma-takeuchi <ma-takeuchi@nextninja.co.jp>
2023-01-27 13:29:48 +0900

    間隔延ばす

--- Source/Service/JenkinsService.cs
+++ Source/Service/JenkinsService.cs
@@ -99,7 +99,7 @@ namespace ChatworkJobTrigger
 
             var runner = new JenkinsJobRunner(client)
             {
-                PollInterval = 2500,
+                PollInterval = 8000,
                 BuildTimeout = setting.JenkinsBuildTimeout,
                 QueueTimeout = setting.JenkinsQueueTimeout,
             };
@@ -145,7 +145,7 @@ namespace ChatworkJobTrigger
 
                         if (build.Building == false) { break; }
                         
-                        await Task.Delay(TimeSpan.FromSeconds(5));
+                        await Task.Delay(TimeSpan.FromSeconds(10));
                     }
                 }
 
